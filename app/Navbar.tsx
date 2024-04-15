ad1e24194fe825f70d7b1d3088527333ca5668b0
opaque <2381794917@qq.com>
2024-04-15 14:10:22 +0800

    Build the new issue page

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -14,8 +14,6 @@ const Navbar = () => {
 
   const pathname = usePathname()
 
-  console.log(pathname)
-
   return (
     <nav className='mb-5 flex h-14 items-center space-x-6 border-b px-5'>
       <Link href='/'>
