2aa45fd009251a6d46b98505d6731bcf43156751
opaque <2381794917@qq.com>
2024-04-16 16:17:52 +0800

    Build an API for  deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -23,3 +23,20 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue)
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) }
+  })
+  if (!issue)
+    return NextResponse.json({ error: 'Invalid issue id' }, { status: 404 })
+
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id) }
+  })
+
+  return NextResponse.json(issue)
+}
