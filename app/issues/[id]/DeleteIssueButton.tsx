4256b81b3e6bbb3402ec42eb9aab211aede11295
opaque <2381794917@qq.com>
2024-04-16 16:18:06 +0800

    Delete issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,12 @@
 'use client'
 
 import { Button, Dialog, Flex } from '@radix-ui/themes'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  const router = useRouter()
+
   return (
     <Dialog.Root>
       <Dialog.Trigger>
@@ -22,7 +26,16 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
             </Button>
           </Dialog.Close>
           <Dialog.Close>
-            <Button color='red'>sure</Button>
+            <Button
+              color='red'
+              onClick={async () => {
+                await axios.delete(`/api/issues/${issueId}`)
+                router.push('/issues')
+                router.refresh()
+              }}
+            >
+              Delete
+            </Button>
           </Dialog.Close>
         </Flex>
       </Dialog.Content>
