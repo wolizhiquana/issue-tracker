8892b0421169b0872b82e33b9ae82f3137f5e947
opaque <2381794917@qq.com>
2024-04-16 15:54:13 +0800

    SHow a confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,33 @@
-import { Issue } from '@prisma/client'
-import { Button } from '@radix-ui/themes'
+'use client'
+
+import { Button, Dialog, Flex } from '@radix-ui/themes'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
-  return <Button color='red'>Delete Issue</Button>
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button color='red'>Delete Issue</Button>
+      </Dialog.Trigger>
+
+      <Dialog.Content>
+        <Dialog.Title>Confirm Deletion</Dialog.Title>
+        <Dialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </Dialog.Description>
+        <Flex justify='end' gap='3' mt='4'>
+          <Dialog.Close>
+            <Button variant='soft' color='gray'>
+              Cancel
+            </Button>
+          </Dialog.Close>
+          <Dialog.Close>
+            <Button color='red'>sure</Button>
+          </Dialog.Close>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
+  )
 }
 
 export default DeleteIssueButton
