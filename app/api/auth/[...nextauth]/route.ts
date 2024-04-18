8fca59c33641d91090abbe9006b5e62285c5ae69
opaque <2381794917@qq.com>
2024-04-18 15:56:09 +0800

    Configure github provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
 import NextAuth from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: []
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: 'b84c98174af0ee6a9d1a',
+      clientSecret: '0e47830226d7fef6a4e42da7e1866eac3ff62569'
+    })
+  ]
 })
-
 
 export const { GET, POST } = handlers
