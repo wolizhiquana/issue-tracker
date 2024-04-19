7d3bac03c70a9dcd38cfe4db25cf806920cb893f
opaque <2381794917@qq.com>
2024-04-19 17:18:45 +0800

    Add assigned issues to prisma schema

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,21 +1,24 @@
 'use client'
 
+import { Skeleton } from '@/app/components'
 import { Select } from '@radix-ui/themes'
+import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { User } from 'next-auth'
-import { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([])
+  const {
+    data: users,
+    error,
+    isLoading
+  } = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then((res) => res.data),
+    staleTime: 60 * 1000, //60s,
+    retry: 3
+  })
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>('/api/users')
-      setUsers(data)
-    }
-
-    fetchUsers()
-  }, [])
+  if (isLoading) return <Skeleton />
 
   return (
     <Select.Root>
@@ -23,7 +26,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id!}>
               {user.name}
             </Select.Item>
