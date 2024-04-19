1d3cbe6de55418d97f4187054ffff17cd2bc9ee3
opaque <2381794917@qq.com>
2024-04-19 18:07:40 +0800

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,20 +1,28 @@
-import { IssueSchema } from '@/app/validationSchemas'
+import { patchIssueSchema } from '@/app/validationSchemas'
+import { auth } from '@/auth'
 import prisma from '@/prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
-import delay from 'delay'
-import { auth } from '@/auth'
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await auth()
-  if (!session) return NextResponse.json({}, { status: 401 })
+  // const session = await auth()
+  // if (!session) return NextResponse.json({}, { status: 401 })
 
   const body = await request.json()
-  const validation = IssueSchema.safeParse(body)
+  const validation = patchIssueSchema.safeParse(body)
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 })
+
+  const { title, description, assignedToUserId } = body
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId }
+    })
+    if (!user)
+      return NextResponse.json({ error: 'Invalid user' }, { status: 400 })
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
@@ -23,7 +31,7 @@ export async function PATCH(
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description }
+    data: { title, description, assignedToUserId }
   })
 
   return NextResponse.json(updatedIssue)
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod'
 
-export const IssueSchema = z.object({
-  title: z.string().min(1, 'title is required').max(255),
-  description: z.string().min(1, 'description is required')
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, 'title is required').max(255).optional(),
+  description: z
+    .string()
+    .min(1, 'description is required')
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, 'AssignedToUserId is required.')
+    .max(255)
+    .optional()
+    .nullable()
 })
