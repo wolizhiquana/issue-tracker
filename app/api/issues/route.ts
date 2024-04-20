45ada117c8696889a9794663df0783e19407243f
opaque <2381794917@qq.com>
2024-04-20 16:05:11 +0800

    Fix create issue bug

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,6 @@
 import prisma from '@/prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
-import { IssueSchema } from '../../validationSchemas'
+import { patchIssueSchema } from '../../validationSchemas'
 import { auth } from '@/auth'
 
 export async function POST(request: NextRequest) {
@@ -8,7 +8,7 @@ export async function POST(request: NextRequest) {
   if (!session) return NextResponse.json({}, { status: 401 })
 
   const body = await request.json()
-  const validation = IssueSchema.safeParse(body)
+  const validation = patchIssueSchema.safeParse(body)
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 })
 
